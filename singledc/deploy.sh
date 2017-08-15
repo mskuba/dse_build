8c3381e65d1e9c1ce454973ef3fd9ff14c4f6fa1
02174619 <mskuba@starbucks.com>
2017-08-15 15:39:17 -0700

    removed hardcoded location from loadbalancer files

--- singledc/deploy.sh
+++ singledc/deploy.sh
@@ -6,12 +6,12 @@ echo "Start Time: $(date)"
 if [ -z "$(which az)" ]
 then
     echo "CLI v2 'az' command not found, falling back to v1 'azure'"
-    azure group create $1 "westus"
+    azure group create $1 "eastus"
     azure group deployment create -f mainTemplate.json -e mainTemplateParameters.json $RESOURCE_GROUP dse
 
 else
     echo "CLI v2 'az' command found"
-    az group create --name $RESOURCE_GROUP --location "westus"
+    az group create --name $RESOURCE_GROUP --location "eastus"
     az group deployment create \
      --resource-group $RESOURCE_GROUP \
      --template-file mainTemplate.json \
