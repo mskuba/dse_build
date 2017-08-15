8674dcbd55427671aecdd9ed393087160e1d0ecb
02174619 <mskuba@starbucks.com>
2017-08-15 16:01:50 -0700

    removed hardcoded location from deploy script

--- singledc/deploy.sh
+++ singledc/deploy.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 RESOURCE_GROUP=$1
+REGION=$2
 
 echo "Start Time: $(date)"
 if [ -z "$(which az)" ]
 then
     echo "CLI v2 'az' command not found, falling back to v1 'azure'"
-    azure group create $1 "eastus"
+    azure group create $1 $2
     azure group deployment create -f mainTemplate.json -e mainTemplateParameters.json $RESOURCE_GROUP dse
 
 else
     echo "CLI v2 'az' command found"
-    az group create --name $RESOURCE_GROUP --location "eastus"
+    az group create --name $RESOURCE_GROUP --location $2
     az group deployment create \
      --resource-group $RESOURCE_GROUP \
      --template-file mainTemplate.json \
@@ -23,7 +24,7 @@ else
      --parameters @lbParameters.json \
      --verbose
 fi
-export $RESOURCE_GROUP
+export RESOURCE_GROUP
 ./bepool_config.sh
 
 echo " "
